e1c2c12263d375b575e57bb086fcc73b6af0e5d1
anie308 <aniebietabasiobo@gmail.com>
2022-04-10 21:15:41 +0100

    initial commit

---
+++ db/index.js
@@ -0,0 +1,4 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://127.0.0.1:27017/bugsmax').then(() => console.log('db connected'))
+.catch(err => console.log('db connection failed: ', err.message || err));
