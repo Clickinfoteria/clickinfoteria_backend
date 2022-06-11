7e1f262f13b07b464580e9866d499e55b9ac9afb
anie308 <aniebietabasiobo@gmail.com>
2022-06-11 04:34:39 +0100

    disabling private routes

--- routers/post.js
+++ routers/post.js
@@ -6,35 +6,35 @@ const { postValidator, validate } = require('../middleware/postValidator');
 const { parseData} = require('../middleware');
 
 
-router.post(
-'/create',
- multer.single( 'thumbnail'),
-parseData,
-postValidator, 
-validate,
- createPost
-);
+// router.post(
+// '/create',
+//  multer.single( 'thumbnail'),
+// parseData,
+// postValidator, 
+// validate,
+//  createPost
+// );
 
-router.put(
-'/:postId',
- multer.single( 'thumbnail'),
-parseData,
-postValidator, 
-validate,
- updatePost
-);
+// router.put(
+// '/:postId',
+//  multer.single( 'thumbnail'),
+// parseData,
+// postValidator, 
+// validate,
+//  updatePost
+// );
 
-router.delete('/:postId', deletePost)
+// router.delete('/:postId', deletePost)
 router.get('/single/:slug', getPost)
 router.get('/featured-posts', getFeaturedPosts)
 router.get('/posts', getPosts)
 router.get('/search', searchPost)
 router.get('/related-posts/:postId', getRelatedPosts)
 
-router.post(
-    '/upload-image',
-     multer.single( 'thumbnail'),
-    uploadImage
-    );
+// router.post(
+//     '/upload-image',
+//      multer.single( 'thumbnail'),
+//     uploadImage
+//     );
 
 module.exports = router;
